65a577c328504679df7f22d41769778b9fd616d2
Ramses Feliciano <ramsesfeliciano@gmail.com>
2024-05-10 16:02:04 -0400

    first commit

---
+++ Circle Objects Lab/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Objects_Lab
+{
+    public class Circle
+    {
+        public double radius { get; set; }
+
+        public Circle(double _radius)
+        {
+            radius = _radius;
+        }
+
+        public double CalculateDiameter(double r)
+        {
+            return r + r;
+        }
+        public double CalculateCircumference(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        public double CalculateArea(double r)
+        {
+            return Math.PI * r * r;
+        }
+        public void Grow()
+        {
+            radius = radius + radius;
+        }
+    }
+}
---
+++ Circle Objects Lab/Program.cs
@@ -0,0 +1,55 @@
+﻿using Circle_Objects_Lab;
+
+bool runProgram = true;
+double input = 0;
+while (true)
+{
+    Console.WriteLine("Enter a radius");
+    try
+    {
+        input = double.Parse(Console.ReadLine());
+        if (input <= 0)
+        {
+            Console.WriteLine("Number can't be negative or 0.");
+            continue;
+        }
+        break;
+    }
+    catch
+    {
+        Console.WriteLine("Invalid input, try again");
+        continue;
+    }
+}
+
+Circle circleresult = new Circle(input);
+while (runProgram)
+{
+
+    Console.WriteLine(circleresult.CalculateDiameter(circleresult.radius));
+    Console.WriteLine(circleresult.CalculateCircumference(circleresult.radius));
+    Console.WriteLine(circleresult.CalculateArea(circleresult.radius));
+    while (true)
+    {
+        Console.WriteLine("Do you want the circle to grow? y/n");
+        string answer = Console.ReadLine();
+        if (answer == "y")
+        {
+            circleresult.Grow();
+            break;
+        }
+        else if (answer == "n")
+        {
+            runProgram = false;
+            Console.WriteLine($"Radius is {circleresult.radius}");
+            Console.WriteLine("Goodbye");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid answer, try again.");
+            continue;
+        }
+    }
+
+}
